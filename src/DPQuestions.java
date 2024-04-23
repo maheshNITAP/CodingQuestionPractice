679de73230dabee5dc5a9767906c1ee0488c710a
maheshNITAP <mm0048511@gmail.com>
2024-04-23 08:14:29 +0530

    Random Question

--- src/DPQuestions.java
+++ src/DPQuestions.java
@@ -822,6 +822,30 @@ public class DPQuestions {
 
         }
     }
+    static class Random{
+
+        public int perfectSquare(int n) {
+            int arr[]=new int[n+1];
+            Arrays.fill(arr,Integer.MAX_VALUE);
+            arr[0]=0;
+            for(int i=1;i<=n;i++){
+                for(int j=1;j*j<=i;j++){
+                    arr[i]=Math.min(arr[i],1+arr[i-(j*j)]);
+                }
+            }
+            return arr[n];
+        }
+
+        public int numberOfWaysToTileAFloor(int n) {
+            int dp[]= new int[n+1];
+            dp[0]=1;
+            dp[1]=1;
+            for(int i=2;i<=n;i++){
+                dp[i]=(dp[i-1]+dp[i-2])%1000000007;
+            }
+            return dp[n];
+        }
+    }
     public static void main(String args[]){
 
         DP dp=new DP();
@@ -1098,12 +1122,26 @@ public class DPQuestions {
 
         //Egg dropping problem recursive+memoization with Optimization
 
-        int e=3;
-        int f=5;
-        int t[][]= new int[e+1][f+1];
-        for(int i=0;i<e+1;i++)
-            Arrays.fill(t[i],-1);
-        System.out.println(mcm.eggDroppingProblemRecursiveWithMemoizationAndOptimization(e,f,t));
+//        int e=3;
+//        int f=5;
+//        int t[][]= new int[e+1][f+1];
+//        for(int i=0;i<e+1;i++)
+//            Arrays.fill(t[i],-1);
+//        System.out.println(mcm.eggDroppingProblemRecursiveWithMemoizationAndOptimization(e,f,t));
+
+
+
+        //Random Questions
+        Random ran= new Random();
+
+
+        //perfect squares
+//        int n=12;
+//        System.out.println(ran.perfectSquare(n));
+
+        //number of ways to tile a floor
+        int n=4;
+        System.out.println(ran.numberOfWaysToTileAFloor(n));
 
 
 
