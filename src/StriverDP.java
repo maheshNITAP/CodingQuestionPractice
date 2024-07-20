c67d677c3e7819583b97e280f4611217bfbcaf72
1308mahesh <112169372+1308mahesh@users.noreply.github.com>
2024-07-20 11:11:41 +0530

    Update StriverDP.java
    print longest common subsequence

--- src/StriverDP.java
+++ src/StriverDP.java
@@ -901,6 +901,38 @@ public class StriverDP {
             }
             return dp[n][m];
         }
+
+        public String printLCS(int n, int m, String x, String y) {
+            int dp[][]= new int[n+1][m+1];
+            for(int i=0;i<n+1;i++)
+                dp[i][0]=0;
+            for(int j=0;j<m+1;j++)
+                dp[0][j]=0;
+            for(int i=1;i<n+1;i++){
+                for(int j=1;j<m+1;j++){
+                    if(x.charAt(i-1)==y.charAt(j-1))
+                        dp[i][j]=1+dp[i-1][j-1];
+                    else
+                        dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+            StringBuilder ans= new StringBuilder();
+            int i=n;
+            int j=m;
+            while (i>0 && j>0){
+                if(x.charAt(i-1)==y.charAt(j-1)){
+                    ans.append(x.charAt(i-1));
+                    i--;j--;
+                }else if(x.charAt(i-1)!= y.charAt(j-1)){
+                    if(dp[i][j-1]>dp[i-1][j]){
+                        j--;
+                    }else {
+                        i--;
+                    }
+                }
+            }
+            return ans.reverse().toString();
+        }
     }
     public static void main(String args[]){
 
@@ -1152,8 +1184,8 @@ public class StriverDP {
 
 //        System.out.println(d.lcsTabulation(n,m,x,y));
 
-
-        
+        //print longest common subsequence
+        System.out.println(d.printLCS(n,m,x,y));
 
 
 
