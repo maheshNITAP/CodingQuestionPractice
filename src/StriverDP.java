6c48a78466f4086a1e2ab5ae124a5473d1cbbb39
1308mahesh <112169372+1308mahesh@users.noreply.github.com>
2024-08-06 09:09:55 +0530

    Update StriverDP.java
    /Count square matrix with all 1's

--- src/StriverDP.java
+++ src/StriverDP.java
@@ -2117,6 +2117,34 @@ public class StriverDP {
                 s.push(new Pair<>(i,v.get(i)));
             }
         }
+
+        public int countSquaresWithAllOnes(int[][] arr) {
+            int n=arr.length;
+            int m=arr[0].length;
+            int dp[][]= new int[n][m];
+
+            for(int i=0;i<n;i++)
+                dp[i][0]=arr[i][0];
+            for(int j=0;j<m;j++)
+                dp[0][j]=arr[0][j];
+
+            for(int i=1;i<n;i++){
+                for (int j=1;j<m;j++){
+                    if(arr[i][j]==0)
+                        dp[i][j]=0;
+                    else {
+                        dp[i][j]=Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
+                    }
+                }
+            }
+            int total=0;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    total+=dp[i][j];
+                }
+            }
+             return total;
+        }
     }
     public static void main(String args[]){
 
@@ -2693,8 +2721,14 @@ public class StriverDP {
         
         
         //Maximum Area Of Histogram
-        int arr[][]={{1,0,1,0,0},{1,0,1,1,1},{1,1,1,1,1},{1,0,0,1,0}};
-        System.out.println(d.MaxAreaOfRectAngleInBinaryMatrix(arr));
+//        int arr[][]={{1,0,1,0,0},{1,0,1,1,1},{1,1,1,1,1},{1,0,0,1,0}};
+//        System.out.println(d.MaxAreaOfRectAngleInBinaryMatrix(arr));
+
+
+        //Count square matrix with all 1's
+
+        int arr[][]={{1,1,1,1},{1,1,1,1},{1,1,1,1}};
+        System.out.println(d.countSquaresWithAllOnes(arr));
         
         
 
