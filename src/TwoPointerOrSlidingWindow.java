26af4cf2d6846d60fa3a03ae5867c29584684ead
1308mahesh <112169372+1308mahesh@users.noreply.github.com>
2024-08-07 17:47:48 +0530

    Create TwoPointerOrSlidingWindow.java
    //Maximum Points You can obtain from cards

---
+++ src/TwoPointerOrSlidingWindow.java
@@ -0,0 +1,80 @@
+public class TwoPointerOrSlidingWindow {
+    static class SW {
+
+        public int constantWindowQuestion(int[] arr, int k) {
+            int n=arr.length;
+            int l=0;
+            int r=k-1;
+            int sum=0;
+            for(int i=0;i<=r;i++)
+                sum+=arr[i];
+            int maxSum=sum;
+            while(r<n-1){
+                sum=sum-arr[l];
+                l++;
+                r++;
+                sum+=arr[r];
+                maxSum=Math.max(sum,maxSum);
+            }
+            return maxSum;
+        }
+
+        public int longestSubArray(int[] arr, int k) {
+            int n=arr.length;
+            int l=0;
+            int r=0;
+            int sum=0;
+            int maxLen=0;
+            while(r<n){
+                sum+=arr[r];
+                if(sum>k){
+                    sum=sum-arr[l];
+                    l++;
+                }
+                if(sum<=k)
+                    maxLen=Math.max(maxLen,r-l+1);
+                r=r+1;
+            }
+            return maxLen;
+        }
+
+        public int maxPointsFromCards(int[] arr, int k) {
+            int n=arr.length;
+            int lIndex=k-1;
+            int rIndex=n-1;
+            int leftSum=0;
+            int rightSum=0;
+            int maxSum=0;
+            for(int i=0;i<k;i++)
+                leftSum+=arr[i];
+            maxSum=Math.max(leftSum,maxSum);
+            for(int i=lIndex;i>=0;i--){
+                leftSum-=arr[i];
+                rightSum+=arr[rIndex];
+                rIndex--;
+                maxSum=Math.max(maxSum,leftSum+rightSum);
+            }
+               return maxSum;
+        }
+    }
+    public static void main(String args[]){
+        SW sw= new SW();
+
+
+        //return the maximum sum of consecutive elements
+//        int arr[]={-1,2,3,3,4,5,-1};
+//        int k=4;
+//        System.out.println(sw.constantWindowQuestion(arr,k));
+
+        //longest subArray or substring with sum <=k
+//        int arr[]={2,5,1,7,10};
+//        int k=14;
+//        System.out.println(sw.longestSubArray(arr,k));
+
+        //Maximum Points You can obtain from cards
+        int arr[]={6,2,3,4,7,2,1,7,1};
+        int k=4;
+        System.out.println(sw.maxPointsFromCards(arr,k));
+
+    }
+}
