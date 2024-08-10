25c4b8f34e756281ae23405353747422770972f8
1308mahesh <112169372+1308mahesh@users.noreply.github.com>
2024-08-10 15:51:20 +0530

    Update TwoPointerOrSlidingWindow.java
    // fruit into basket

--- src/TwoPointerOrSlidingWindow.java
+++ src/TwoPointerOrSlidingWindow.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 
 public class TwoPointerOrSlidingWindow {
@@ -118,6 +120,67 @@ public class TwoPointerOrSlidingWindow {
             }
             return maxLen;
         }
+
+
+        //brute force
+        public int maxLengthOfSubArrayWithAtMostKTypesOfNumbers(int[] arr,int k) {
+            int n=arr.length;
+            int maxLen=0;
+            for(int i=0;i<n;i++){
+                HashSet<Integer> st= new HashSet<>();
+                for(int j=i;j<n;j++){
+                    st.add(arr[j]);
+                    if(st.size()<=k)
+                        maxLen=Math.max(maxLen,j-i+1);
+                    else break;
+                }
+            }
+            return maxLen;
+        }
+
+
+        //two pointer
+        public int maxLengthOfSubarrayWithAtmostKTypesOfNumbersTwoPointer(int[] arr, int k) {
+            int r=0,l=0, maxLen=0;
+            HashMap<Integer,Integer> map= new HashMap<>();
+            while(r<arr.length){
+                if(map.containsKey(arr[r]))
+                    map.put(arr[r],map.get(arr[r])+1);
+                else
+                    map.put(arr[r],1);
+                while(map.size()>k){
+                    map.put(arr[l],map.get(arr[l])-1);
+                    if(map.get(arr[l])==0)
+                        map.remove(arr[l]);
+                    l++;
+                }
+                if(map.size()<=k)
+                    maxLen=Math.max(maxLen,r-l+1);
+                r++;
+            }
+            return maxLen;
+        }
+
+        public int maxLengthOfSubarrayWithAtmostKTypesOfNumbersSlidingWindow(int[] arr, int k) {
+            int l=0,r=0,maxLen=0;
+            HashMap<Integer,Integer> map= new HashMap<>();
+            while(r<arr.length){
+                if(map.containsKey(arr[r]))
+                    map.put(arr[r],map.get(arr[r])+1);
+                else
+                    map.put(arr[r],1);
+                if(map.size()>k){
+                    map.put(arr[l],map.get(arr[l])-1);
+                    if(map.get(arr[l])==0)
+                        map.remove(arr[l]);
+                    l++;
+                }
+                if(map.size()<=k)
+                    maxLen=Math.max(maxLen,r-l+1);
+                r++;
+            }
+            return maxLen;
+        }
     }
     public static void main(String args[]){
         SW sw= new SW();
@@ -143,8 +206,8 @@ public class TwoPointerOrSlidingWindow {
 //        System.out.println(sw.longestSubStringWithoutRepeatingChar(s));
 
         //Maximum consecutive 1's with at most k zeros  Or if you can flip k zeros
-        int arr[]={1,1,1,0,0,0,1,1,1,1,0};
-        int k=2;//O/P-6
+//        int arr[]={1,1,1,0,0,0,1,1,1,1,0};
+//        int k=2;//O/P-6
 
 //        int arr[]={0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
 //        int k=3; //O/P-10
@@ -152,7 +215,22 @@ public class TwoPointerOrSlidingWindow {
 //        with two pointer TC-O(N+N)
 //        System.out.println(sw.maxConsecutiveOnes(arr,k));
 
-        System.out.println(sw.maxConsecutiveOnesWithFixedSlidingWindow(arr,k));
+        //with sliding window
+//        System.out.println(sw.maxConsecutiveOnesWithFixedSlidingWindow(arr,k));
+
+        // fruit into basket
+
+        int arr[]={3,3,3,1,2,1,1,2,3,3,4};
+        int k=2;//number of basket
+
+        //brute force
+//        System.out.println(sw.maxLengthOfSubArrayWithAtMostKTypesOfNumbers(arr,k));
+
+        //two pointer
+//        System.out.println(sw.maxLengthOfSubarrayWithAtmostKTypesOfNumbersTwoPointer(arr,k));
+
+        //sliding window
+        System.out.println(sw.maxLengthOfSubarrayWithAtmostKTypesOfNumbersSlidingWindow(arr,k));
 
 
 
