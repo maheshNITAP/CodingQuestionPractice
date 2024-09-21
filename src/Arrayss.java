f6cd690a445c3f451464ca62030c3e6a2c5db4ab
1308mahesh <112169372+1308mahesh@users.noreply.github.com>
2024-09-21 09:40:41 +0530

    Update Arrayss.java
     //set matrix zeros--set all rows and column to zeros which consist 0 in it

--- src/Arrayss.java
+++ src/Arrayss.java
@@ -1,8 +1,5 @@
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
-import javafx.beans.property.adapter.ReadOnlyJavaBeanBooleanProperty;
 import javafx.util.Pair;
 
-import java.net.Inet4Address;
 import java.util.*;
 
 
@@ -679,6 +676,60 @@ public class Arrayss {
             return largest;
 
         }
+
+        public void setMatrixZeros(int[][] arr) {
+            int n=arr.length;
+            int m=arr[0].length;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(arr[i][j]==0){
+                        makeRowZero(arr,i,m);
+                        makeColZero(arr,j,n);
+                    }
+                }
+            }
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(arr[i][j]==-1)
+                        arr[i][j]=0;
+                }
+            }
+        }
+
+        private void makeColZero(int[][] arr, int j, int n) {
+            for(int i=0;i<n;i++){
+                if(arr[i][j]!=-0){
+                    arr[i][j]=-1;
+                }
+            }
+        }
+
+        private void makeRowZero(int[][] arr, int i, int m) {
+            for(int j=0;j<m;j++){
+                if(arr[i][j]!=0)
+                    arr[i][j]=-1;
+            }
+        }
+
+        public void setMatrixZerosBetterSol(int[][] arr) {
+            int n=arr.length;
+            int m=arr[0].length;
+            int col[]= new int[m],row[]= new int[n];
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(arr[i][j]==0){
+                        row[i]=1;
+                        col[j]=1;
+                    }
+                }
+            }
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(row[i]==1 || col[j]==1)
+                        arr[i][j]=0;
+                }
+            }
+        }
     }
     public static void main(String args[]){
         ArrayQuestions array= new ArrayQuestions();
@@ -906,7 +957,7 @@ public class Arrayss {
 
         //Longest Consecutive Sequence
 
-        int arr[]={102,4,100,1,101,3,2,1,1,103,104};
+//        int arr[]={102,4,100,1,101,3,2,1,1,103,104};
 
         //brute force
 //        System.out.println(array.longestConsecutiveSequenceBruteForce(arr));
@@ -915,7 +966,29 @@ public class Arrayss {
 //        System.out.println(array.longestConsecutiveSequenceBetterSol(arr));
 
         //Optimal Solution
-        System.out.println(array.longestConsecutiveSequenceOptimalSol(arr));
+//        System.out.println(array.longestConsecutiveSequenceOptimalSol(arr));
+
+        //set matrix zeros--set all rows and column to zeros which consist 0 in it
+
+        int arr[][]={{1,1,1,1},{1,0,0,1},{1,1,0,1},{1,1,1,1}};
+
+        //brute force
+//        array.setMatrixZeros(arr);
+//
+//        Arrays.stream(arr).forEach(e-> {
+//            Arrays.stream(e).forEach(System.out::print);
+//            System.out.println(" ");
+//        });
+
+        //Better Solution
+        array.setMatrixZerosBetterSol(arr);
+        Arrays.stream(arr).forEach(e-> {
+            Arrays.stream(e).forEach(System.out::print);
+            System.out.println(" ");
+        });
+
+        //Optimal---it's there you can check notes
+
 
 
 
