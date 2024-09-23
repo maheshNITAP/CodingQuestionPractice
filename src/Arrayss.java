04be916e7976c2251ba9d1d0d028f7a0d553b36a
1308mahesh <112169372+1308mahesh@users.noreply.github.com>
2024-09-23 09:55:11 +0530

    Update Arrayss.java
    //Spiral traversal of a matrix

--- src/Arrayss.java
+++ src/Arrayss.java
@@ -758,6 +758,38 @@ public class Arrayss {
             arr[i][j]=arr[j][i];
             arr[j][i]=t;
         }
+
+        public void spiralTraversalOfMatrix(int[][] arr) {
+            int n=arr.length;
+            int m=arr[0].length;
+
+            int top=0,left=0,right=m-1,bottom=n-1;
+            while(top<=bottom && left<=right){
+
+                //right-->
+                for(int i=left;i<=right;i++)
+                    System.out.print(arr[top][i]+" ");
+                top++;
+                //bottom--(top to bottom)
+                for(int i=top;i<=bottom;i++)
+                    System.out.print(arr[i][right]+" ");
+                right--;
+
+                //left---- <--
+                if(top<=bottom){
+                    for(int i=right;i>=left;i--)
+                        System.out.print(arr[bottom][i]+" ");
+                    bottom--;
+                }
+
+                //top
+                if(bottom<=right){
+                    for(int i=bottom;i>=top;i--)
+                        System.out.print(arr[i][left]+" ");
+                    left++;
+                }
+            }
+        }
     }
     public static void main(String args[]){
         ArrayQuestions array= new ArrayQuestions();
@@ -1019,12 +1051,12 @@ public class Arrayss {
 
 
         //rotate matrix/Image by 90 degrees
-        int arr[][]={
-                {1,2,3,4},
-                {5,6,7,8},
-                {9,10,11,12},
-                {13,14,15,16}
-        };
+//        int arr[][]={
+//                {1,2,3,4},
+//                {5,6,7,8},
+//                {9,10,11,12},
+//                {13,14,15,16}
+//        };
 
         //brute force with extra space
 //        int[][] ans = array.rotateMatrixBy90DegreesBruteForce(arr);
@@ -1035,11 +1067,24 @@ public class Arrayss {
 //        });
 
         //Optimal using transpose matrix
-        array.rotateMatrixBy90DegreesOptimal(arr);
-                Arrays.stream(arr).forEach(e-> {
-            Arrays.stream(e).forEach(System.out::print);
-            System.out.println(" ");
-        });
+//        array.rotateMatrixBy90DegreesOptimal(arr);
+//                Arrays.stream(arr).forEach(e-> {
+//            Arrays.stream(e).forEach(System.out::print);
+//            System.out.println(" ");
+//        });
+
+
+        //Spiral traversal of a matrix
+        int arr[][]={
+                {1,2,3,4,5,6},
+                {20,21,22,23,24,7},
+                {19,32,33,34,25,8},
+                {18,31,36,35,26,9},
+                {17,30,29,28,27,10},
+                {16,15,14,13,12,11}};
+
+        array.spiralTraversalOfMatrix(arr);
+
 
 
 
