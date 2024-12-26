4f79967e6b9ebc9460cdf3f2c193b51270150e77
1308mahesh <112169372+1308mahesh@users.noreply.github.com>
2024-12-26 09:06:04 +0530

    Update BinarySearchByStriver.java
    ///search minimum element in rotated sorted array

--- src/BinarySearchByStriver.java
+++ src/BinarySearchByStriver.java
@@ -47,6 +47,28 @@ public class BinarySearchByStriver {
         return false;
     }
 
+    private int searchMinimumElementInRotatedSortedArray(int[] arr) {
+        int low=0,high=arr.length-1;
+        int ans=Integer.MAX_VALUE;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            //for Optimization
+            if(arr[low]<=arr[high]){//if full array itself is sorted then no need to do BS again and again we will get min in first time itself--
+                ans=Math.min(ans,arr[low]);
+                break;
+            }
+            if(arr[low]<=arr[mid]){
+                ans=Math.min(ans,arr[low]);
+                low=mid+1;
+            }else{
+                ans=Math.min(ans,arr[mid]);
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String args[]){
         BinarySearchByStriver bs=new BinarySearchByStriver();
 
@@ -56,12 +78,18 @@ public class BinarySearchByStriver {
 //        System.out.println(bs.searchElementInRotatedSortedArray(arr,target));
 
         //search element in rotated sorted Array with duplicate elements-- if present return true or false
-        int arr[]={3,1,2,3,3,3,3};
-        int target =1;
-        System.out.println(bs.searchElementInRotatedSortedArrayWithDuplicateElement(arr,target));
+//        int arr[]={3,1,2,3,3,3,3};
+//        int target =1;
+//        System.out.println(bs.searchElementInRotatedSortedArrayWithDuplicateElement(arr,target));
+
+        ///search minimum element in rotated sorted array
+        int arr[]={4,5,6,7,0,1,2};
+        System.out.println(bs.searchMinimumElementInRotatedSortedArray(arr));
 
 
     }
+
+
 
 
 }
