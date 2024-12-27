1c7a1857574806bc0e1d37e1d22aa5df44b61e09
1308mahesh <112169372+1308mahesh@users.noreply.github.com>
2024-12-27 10:32:14 +0530

    Update BinarySearchByStriver.java
    //find any Peak Element--multiple peak and single peak both

--- src/BinarySearchByStriver.java
+++ src/BinarySearchByStriver.java
@@ -136,6 +136,35 @@ public class BinarySearchByStriver {
         }
         return -1;
     }
+
+    private int findPeakElementByBruteForce(int[] arr) {
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if((i==0 || arr[i-1]<arr[i]) && (i==n-1 || arr[i]>arr[i+1]))
+                return i;
+        }
+        return -1;
+    }
+
+    private int findPeakElementByBinarySearch(int[] arr) {
+        int n=arr.length;
+        if(n==1) return 0;
+        if(arr[0]>arr[1]) return 0;
+        if(arr[n-1]>arr[n-1]) return n-1;
+        int low=1,high=n-2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
+                return mid;
+            else if(arr[mid]>arr[mid-1])
+                low=mid+1;
+//            else if(arr[mid]>arr[mid+1])//--if we use this the it will only work for single peak
+//                high=mid-1;
+            else //instead of else if if we use only else then it will work for multiple peak as well
+                high=mid-1;
+        }
+        return -1;
+    }
     public static void main(String args[]){
         BinarySearchByStriver bs=new BinarySearchByStriver();
 
@@ -158,13 +187,22 @@ public class BinarySearchByStriver {
 //        System.out.println(bs.findHowManyTimesAnArrayIsRotated(arr));
 
         //find single element in Sorted array
-        int arr[]={1,1,2,2,3,3,4,5,5,6,6};
+        //int arr[]={1,1,2,2,3,3,4,5,5,6,6};
 
         //by linear search
         //System.out.println(bs.findSingleElementInSortedArrayByLinearSearch(arr));
 
         //By binary search
-        System.out.println(bs.findSingleElementInSortedArrayByBinarySearch(arr));
+        //System.out.println(bs.findSingleElementInSortedArrayByBinarySearch(arr));
+
+
+        //find any Peak Element--multiple peak and single peak both
+//        int arr[]={1,2,3,4,5,6,7,8,5,1};//--8 at 7th index so output is 7
+        int arr[]={1,5,1,2,1};
+        //brute force
+//        System.out.println(bs.findPeakElementByBruteForce(arr));
+
+        System.out.println(bs.findPeakElementByBinarySearch(arr));
 
 
 
