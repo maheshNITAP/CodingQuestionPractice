d87045ce460f64e5b1c55fabd83fde7d184e1b6e
1308mahesh <112169372+1308mahesh@users.noreply.github.com>
2024-12-31 13:04:05 +0530

    Update BinarySearchByStriver.java
    //Median of two sorted arrays of Different Sizes

--- src/BinarySearchByStriver.java
+++ src/BinarySearchByStriver.java
@@ -1,5 +1,6 @@
 import javafx.util.Pair;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
@@ -563,6 +564,125 @@ public class BinarySearchByStriver {
             return pq.peek().getKey();
 
         }
+
+        public int medianOfTwoSortedArraysOfDifferentSizesBruteForceWithExtraSpace(int[] arr1, int[] arr2) {
+            int n1= arr1.length;
+            int n2=arr2.length;
+
+            int n=n1+n2;
+            ArrayList<Integer> arr= new ArrayList<>();
+            int i=0,j=0;
+            while(i<n1 && j<n2){
+                if(arr1[i]<arr2[j])
+                    arr.add(arr1[i++]);
+                else
+                    arr.add(arr2[j++]);
+            }
+            while(i<n1)
+                arr.add(arr1[i++]);
+            while(j<n2)
+                arr.add(arr2[j++]);
+
+            if(n%2==1)
+                return arr.get(n/2);
+            return (arr.get(n/2)+arr.get((n/2)-1))/2;
+        }
+
+        public int medianOfTwoSortedArraysOfDifferentSizesBruteForceWithOutExtraSpace(int[] arr1, int[] arr2) {
+            int n1=arr1.length;
+            int n2=arr2.length;
+            int n=n1+n2;
+            int ind2=n/2;
+            int ind1= ind2-1;
+            int count=0;
+            int ind1ele=-1,ind2ele=-1;
+            int i=0,j=0;
+            while(i<n1 && j<n2){
+                if(arr1[i]<arr2[j]){
+                    if(count==ind1) ind1ele=arr1[i];
+                    if(count==ind2) ind2ele=arr1[i];
+                    count++;
+                    i++;
+                }else{
+                    if(count==ind1)ind1ele=arr2[j];
+                    if(count==ind2)ind2ele=arr2[j];
+                    count++;
+                    j++;
+                }
+            }
+            while(i<n1){
+                if(count==ind1)ind1ele=arr1[i];
+                if(count==ind2)ind2ele=arr1[i];
+                count++;
+                i++;
+                count++;
+            }
+            while(j<n2){
+                if(count==ind1) ind1ele=arr2[j];
+                if(count==ind2) ind2ele=arr2[j];
+                j++;
+                count++;
+            }
+            if(n%2==1)
+                return ind2ele;
+            return (ind1ele+ind2ele)/2;
+        }
+
+        public double medianOfTwoSortedArraysOfDifferentSizesByBinarySearch(int[] arr1, int[] arr2) {
+            int n1=arr1.length;
+            int n2=arr2.length;
+            if(n1>n2) return medianOfTwoSortedArraysOfDifferentSizesByBinarySearch(arr2,arr1);//just calling again this fun if arr1 is larger than arr2
+            int low=0,high=n1;
+            int left=(n1+n2+1)/2;
+            int n=n1+n2;
+            while(low<=high){
+                int mid1=(low+high)>>1;
+                int mid2=left-mid1;
+                int l1=Integer.MIN_VALUE, l2=Integer.MIN_VALUE,r1=Integer.MAX_VALUE,r2=Integer.MAX_VALUE;
+                if(mid1<n1) r1=arr1[mid1];
+                if(mid2<n2) r2=arr2[mid2];
+
+                if(mid1-1>=0) l1=arr1[mid1-1];
+                if(mid2-1>=0) l2=arr2[mid2-1];
+
+                if(l1<=r2 && l2<=r1){
+                    if(n%2==1)
+                        return Math.max(l1,l2);
+                    return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+                }else if(l1<l2)
+                    high=mid1-1;
+                else
+                    low=mid1+1;
+            }
+            return -1;
+        }
+
+        public int kthElementOfTwoSortedArrayByBinarySearch(int[] arr1, int[] arr2, int k) {
+            int n1= arr1.length;
+            int n2=arr2.length;
+            int n=n1+n2;
+            int left=k;
+            int low=Math.max(k-n2,0), high=Math.min(k,n1);
+            while(low<=high){
+                int mid1=(low+high)/2;
+                int mid2=left-mid1;
+                int l1=Integer.MIN_VALUE,l2=Integer.MIN_VALUE,r1=Integer.MAX_VALUE,r2=Integer.MAX_VALUE;
+                if(mid1<n1) r1=arr1[mid1];
+                if(mid2<n2) r2=arr2[mid2];
+
+                if(mid1-1>=0) l1=arr1[mid1-1];
+                if(mid2-1>=0) l2=arr2[mid2-1];
+                if(l1<=r2 && l2<=r1)
+                    return Math.max(l1,l2);
+                else if(l1>r2)
+                    high=mid1-1;
+                else
+                    low=mid1+1;
+
+            }
+            return 0;
+
+        }
     }
 
     public static void main(String args[]){
@@ -697,18 +817,44 @@ public class BinarySearchByStriver {
 
 
         //Minimise Maximum Distance Between Gas Station
-        int arr[]={1,13,17,23};
-        int k=5;//----O/P--3
+//        int arr[]={1,13,17,23};
+//        int k=5;//----O/P--3
 
 //        System.out.println(bsoa.minimiseMaximumDistanceBetweenGasStationsBruteForce(arr,k));
 
-        System.out.println(bsoa.minimiseMaximumDistanceBetweenGasStationsOptimalByHeap(arr,k));
+        //System.out.println(bsoa.minimiseMaximumDistanceBetweenGasStationsOptimalByHeap(arr,k));
+
+
+        //Median of two sorted arrays of Different Sizes
+
+//        int arr1[]={1,3,4,7,10,12};
+//        int arr2[]={2,3,6,15};
+
+        //Brute force with Extra space
+//        System.out.println(bsoa.medianOfTwoSortedArraysOfDifferentSizesBruteForceWithExtraSpace(arr1,arr2));
+
+        //Brute force without Extra spce
+//        System.out.println(bsoa.medianOfTwoSortedArraysOfDifferentSizesBruteForceWithOutExtraSpace(arr1,arr2));
+
+
+        //NEED TO REVISIT
+        //with Binary search
+//        System.out.println(bsoa.medianOfTwoSortedArraysOfDifferentSizesByBinarySearch(arr1,arr2));
+
+        //Kth Element of two sorted Arrays
+        //--same concept as Median of two sorted arrays of Different Sizes only instead of median we need to find kth element
+
+        int arr1[]={2,3,6,7,9};
+        int arr2[]={1,4,8,10};
+
+        int k=4;
+        System.out.println(bsoa.kthElementOfTwoSortedArrayByBinarySearch(arr1,arr2,k));
+
+
 
 
 
     }
-
-
 
 
 }
