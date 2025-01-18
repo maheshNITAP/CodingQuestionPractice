a5bffdfa61c4b5694d8c5d6ce2d342d5780ba656
1308mahesh <112169372+1308mahesh@users.noreply.github.com>
2025-01-18 07:17:10 +0530

    Update StringQuestions.java
    Check if the Sentence Is Pangram

--- src/StringQuestions.java
+++ src/StringQuestions.java
@@ -2,6 +2,7 @@ public class StringQuestions {
     //String used functions
     //Integer to String---->String s=Integer.toString(123);
     //print subArray from index i to some length ---->String.valueOf(s,0,ind)
+    //String to charArray---> char ch[]=s.toCharArray()
     static class SQ{
 
         public int stringCompression(char[] chars) {
@@ -20,21 +21,43 @@ public class StringQuestions {
                 chars[index++]=currChar;
                 if(currCount>1){
                     String s=Integer.toString(currCount);
-                    for(int k=0;k<s.length();k++){
-                        chars[index++]=s.charAt(k);
-                    }
+//                    for(int k=0;k<s.length();k++){
+//                        chars[index++]=s.charAt(k);
+//                    }
+                    for(char c:s.toCharArray())
+                        chars[index++]=c;
                 }
             }
             return index;
+        }
+
+        public boolean checkIfTheSentenceIsPangram(String s) {
+            int hash[]= new int[26];
+            int count=0;
+            for(int i=0;i<s.length();i++){
+                hash[s.charAt(i)-'a']++;
+                if(hash[s.charAt(i)-'a']==1){
+                    count++;
+                    if(count==26)
+                        return true;
+                }
+            }
+            return false;
         }
     }
     public static void main(String args[]){
         SQ sq= new SQ();
 
         //String Compression-------leetcode--443
-        char[] s={'a','a','a','b','b','c','d','d','d','d'};
-        int ind=sq.stringCompression(s);
-        System.out.println("size of ans is :"+ind+" ans string is :"+String.valueOf(s,0,ind));
+//        char[] s={'a','a','a','b','b','c','d','d','d','d'};
+//        int ind=sq.stringCompression(s);
+//        System.out.println("size of ans is :"+ind+" ans string is :"+String.valueOf(s,0,ind));
+
+//        Check if the Sentence Is Pangram
+
+//        String s="thequickbrownfoxjumpsoverthelazydog"
+        String s="leetcode";
+        System.out.println(sq.checkIfTheSentenceIsPangram(s));
     }
 
 }
